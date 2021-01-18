ff423576a7d287a1f844cdae9fe17259eee5f565
Alexie01 <62515223+Alexie81@users.noreply.github.com>
2021-01-18 23:58:29 +0200

    Delete sfarsit.js

--- sfarsit.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    swal({
-        title: 'PARMA END TOUR',
-        text: 'Acesta este finalul turului orasului PARMA. Speram ca va placut ! Pentru a iesi din acesta pagina, apasati ok, iar apoi, apasati semnul "X". Va multumim pentru vizita !',
-        icon: 'success'
-      });
-}
